c4d48dbe0a8f54dfe4cda99add51c5a0d23da036
jhleee <ng0301@gmail.com>
2024-07-05 16:09:21 +0900

    fix: missing assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useMemo, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import SalaryChart from './components/SalaryChart';
 import Slider from './components/Slider';
